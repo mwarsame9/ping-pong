0afca8c7515c54adecf4c9d66adc42301c408494
Mohamed Warsame <mwarsame9@gmail.com>
2017-03-31 13:56:21 -0700

    commit changes that keep what I had.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     var userInput = $("input#numberInput").val();
+    var display = ""
 
+// var game = []
+//   game.forEach(function (userInput) {
 
     for ( var index = 1; index <= userInput; index += 1) {
-
-          if (index % 15 === 0) {
+      // value = index
+        if ((index % 5 === 0) && (index % 3 === 0)) {
+            // value = "pingpong";
             console.log("pingpong");
 
         } else if (index % 5 === 0) {
+            //  value = "pong";
              console.log("pong");
 
         } else if (index % 3 === 0) {
+            // value = "ping";
             console.log("ping");
-
         } else {
             console.log(index);
         }
-      };
 
-    $("#output").text(index);
+
+    };
+
+// });
 
     event.preventDefault();
   });
