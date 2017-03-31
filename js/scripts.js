994bee2ec24603c7a869174434f7a62870ac6041
Mohamed Warsame <mwarsame9@gmail.com>
2017-03-31 15:59:47 -0700

    add logic that returns all values in a list, add image background and finish css styling.

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,26 @@ $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#numberInput").val();
-    var display = ""
-
-// var game = []
-//   game.forEach(function (userInput) {
 
     for ( var index = 1; index <= userInput; index += 1) {
-      // value = index
-        if ((index % 5 === 0) && (index % 3 === 0)) {
-            // value = "pingpong";
-            // console.log("pingpong");
-            $("#result").append("pingpong");
-            
+
+        if (index % 15 === 0) {
+
+            $("#result").append("ping-pong");
+            $("#result").append("<br/>");
         } else if (index % 5 === 0) {
-            //  value = "pong";
-            //  console.log("pong");
+
              $("#result").append("pong");
-            
+             $("#result").append("<br/>");
 
         } else if (index % 3 === 0) {
-            // value = "ping";
-            // console.log("ping");
+
             $("#result").append("ping");
-            
+            $("#result").append("<br/>");
         } else {
-            // console.log(index);
+
             $("#result").append(index);
-            
+            $("#result").append("<br/>");
         }
 
 
